fad3cd5d4518c88917084701e0cdf7001e261110
Luis <luisinhofehalcantara@gmail.com>
2025-12-03 22:49:31 -0300

    initial commit

---
+++ src/exercicio.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const multiplicação = (a, b) => {
+    return a * b;
+};
+console.log(multiplicação(10, 20));
+const saudacao = (nome) => {
+    return `Olá ${nome}`;
+};
+console.log(saudacao('Luis Fernando'));
---
+++ src/exercicio.ts
@@ -0,0 +1,11 @@
+const multiplicação = (a:number,b:number):number=>{
+    return a*b
+}
+
+console.log(multiplicação(10,20))
+
+const saudacao = (nome:string ):string=>{
+    return `Olá ${nome}`
+}
+
+console.log(saudacao('Luis Fernando'));
